3c3e0db14949ee82d495ef3373d66977a7df69e6
Menelik Tawye <tikursew3@gmail.com>
2024-09-10 00:02:51 +0000

    Largest Sum Completed

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -10,6 +10,36 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        /* 
+        Integer largestSum = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if(nums.get(i) + nums.get(j) > largestSum) {
+                    largestSum = nums.get(i) + nums.get(j);
+                }
+            }         
+        }
+        return largestSum;
+        */
+        Integer first;
+        Integer second;
+        if(nums.get(0) > nums.get(1)){
+            first = nums.get(0);
+            second = nums.get(1);
+        }else {
+            first = nums.get(1);
+            second = nums.get(0);
+        }
+        for (int k = 2; k < nums.size(); k++) {
+            if(nums.get(k) > first) {
+                second = first;
+                first = nums.get(k);
+            } else if (nums.get(k) > second) {
+                second = nums.get(k);
+            }
+        }
+        return first + second;
     }
+
+
 }
